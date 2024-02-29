6d989781103f3535f90ae8862cca797e639ab0b3
litepost <litepost@gmail.com>
2024-02-28 21:02:20 -0800

    repository, domain, DbContext

---
+++ DataAccess/SocialDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class SocialDbContext(DbContextOptions options) : DbContext(options)
+{
+    DbSet<Post> Posts { get; set; }
+}
