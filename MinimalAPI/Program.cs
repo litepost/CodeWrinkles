b7a4c88fba182d775564129556c50e6611385f14
litepost <litepost@gmail.com>
2024-03-06 20:35:53 -0800

    working through MinimalAPI routes

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,6 +1,10 @@
 using Application;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +14,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<SocialDbContext>(options => options.UseSqlite("Data source=MinimalAPICourse.db"));
 builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
 var app = builder.Build();
 
@@ -20,5 +25,28 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/api/posts/{id}", async (IMediator mediator, int id) => 
+{
+    var getPost = new GetPostById { PostId = id };
+    var post = await mediator.Send(getPost);
+    return Results.Ok(post);
+})
+.WithName("GetPostById");
+
+app.MapGet("api/posts", async (IMediator mediator) => 
+{
+    var getCommamd = new GetAllPosts();
+    var posts = await mediator.Send(getCommamd);
+    return Results.Ok(posts);
+});
+
+app.MapPost("/api/posts", async (IMediator mediator, Post post) => 
+{
+    var createPost = new CreatePost { PostContent = post.Content };
+    var createdPost = await mediator.Send(createPost);
+    // GetPostById references the route name above so they must match
+    return Results.CreatedAtRoute("GetPostById", new { createdPost.Id }, createdPost);
+});
 
 app.Run();
